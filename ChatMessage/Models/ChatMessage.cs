0c6757a8a3e0e805f88ef5a7b0bc0f3f02b15e70
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2023-11-06 02:42:08 +0100

    Update ChatMessage.cs

--- ChatMessage/Models/ChatMessage.cs
+++ ChatMessage/Models/ChatMessage.cs
@@ -1,8 +1,12 @@
-﻿namespace ChatMessageManager.Models
+// Den repræsenterer en chatbesked med tekst og følelse.
+namespace ChatMessageManager.Models
 {
     public class ChatMessage
     {
+        // Dette er en egenskab for selve teksten i chatbeskeden.
         public string Text { get; set; }
+
+        // Og dette er en egenskab for følelsen (sentiment) i chatbeskeden.
         public string Sentiment { get; set; }
     }
 }
