b2abbfcec46213e78cc099dec8ee3166c69bd63e
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2023-11-06 02:43:32 +0100

    Update MainWindow.xaml.cs

--- ChatMessage/Views/MainWindow.xaml.cs
+++ ChatMessage/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+// Den er ansvarlig for brugergrænsefladen og datahåndtering i vores WPF-applikation.
+using System.Windows;
 using ChatMessageManager.Helpers;
 using ChatMessageManager.Models;
 using System.Collections.Generic;
@@ -8,24 +9,26 @@ namespace ChatMessageManager
     public partial class MainWindow : Window
     {
         private XmlParser xmlParser;
-        private const string XmlFilePath = "Data/chatdata.xml"; // Henvisning XML filen
+        private const string XmlFilePath = "Data/chatdata.xml"; // Dette er stien til vores XML-fil
 
+        // Dette er konstruktøren for MainWindow, hvor vi initialiserer komponenterne og indlæser chatbeskeder fra XML-filen.
         public MainWindow()
         {
             InitializeComponent();
             xmlParser = new XmlParser(XmlFilePath);
-            List<ChatMessage> chatMessages = xmlParser.ParseXml();                                                                                                                      
+            List<ChatMessage> chatMessages = xmlParser.ParseXml();
 
-            // HUmør henvisningerne 
+            // Her filtrerer vi chatbeskeder med humørerne "happy," "neutral," eller "sad."
             List<ChatMessage> happyMessages = new List<ChatMessage>();
             foreach (ChatMessage message in chatMessages)
             {
-                if (message.Sentiment == "happy, neutral, angry")
+                if (message.Sentiment == "happy, neutral, sad")
                 {
                     happyMessages.Add(message);
                 }
             }
 
+            // Vi sætter datakilden for ListView til de filtrerede chatbeskeder.
             ChatMessagesListView.ItemsSource = happyMessages;
         }
     }
